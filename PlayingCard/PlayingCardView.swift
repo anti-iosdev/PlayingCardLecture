309020ae78f5cf3c57eb5b713ee018452b7b3632
Anti <anti@antis-imac-pro.local>
2018-10-01 07:32:29 -0700

    more setup

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Anti on 10/1/18.
+//  Copyright © 2018 Anti. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
